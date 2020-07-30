16ebd727bedc29ed038f10c3103a4cdf2d6c9526
Ivan Santos <untalivan@isantosp.com>
2020-07-30 14:40:20 -0500

    return url

---
+++ payment.php
@@ -0,0 +1,28 @@
+<?php
+
+require __DIR__ .  '/vendor/autoload.php';
+
+MercadoPago\SDK::setAccessToken('APP_USR-1159009372558727-072921-8d0b9980c7494985a5abd19fbe921a3d-617633181');
+
+switch($_GET['res']){
+    case 'failure':
+        echo '<h1>Pago RECHAZADO!</h1>';
+        break;
+
+    case 'pending':
+        echo '<h1>Pago PENDIENTE o EN PROCESO!</h1>';
+        break;
+
+    case 'success':
+        echo '<h1>Pago EXITOSO!</h1>';
+        echo '<hr>';
+        echo '<strong>external_reference</strong>: '.$_GET['external_reference'].'<br>';
+        echo '<strong>collection_id</strong>: '.$_GET['collection_id'].'<br>';
+        echo '<strong>payment_type</strong>: '.$_GET['payment_type'].'<br>';
+
+        $payment = MercadoPago\Payment::find_by_id($_GET['collection_id']);
+        echo '<strong>payment_method_id</strong>: '.$payment->payment_method_id.'<br>';
+        break;
+}
+
+?>
